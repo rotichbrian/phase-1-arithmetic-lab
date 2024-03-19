57e879b52e89dff27a8abfad73ff78dcac4c0f5a
Brian Kipkorir <rotichbrian891@gmail.com>
2024-03-19 13:22:48 +0300

    completed Arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);  // Output: 62
+
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);  // Output: Random integer greater than 0
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);  // Output: 0 (since 20 divided by 4 leaves no remainder)
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max);  // Output: 20
+
